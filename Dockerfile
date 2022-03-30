42324191a551316a83ba5989e270dd8492a34947
diego-bellon <difer2085@gmail.com>
2022-03-30 11:57:50 -0500

    feat: SDP configuration in container

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu
+FROM amazonlinux
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod a+x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,23 @@
 #!/bin/sh -l
-echo "username $1"
-echo "password $2"
-echo "secret-id $3"
-time=$(date)
+sudo amazon-linux-extras install -y epel &&\
+sudo yum install -y https://repo.ius.io/ius-release-el7.rpm && \
+sudo yum install dnsmasq python36 python36-pip python36-dbus && \
+wget https://bin.appgate-sdp.com/5.3/client/appgate-sdp-headless-5.3.3-el7.x86_64.rpm && \
+sudo rpm --install -y --nodeps ./appgate-sdp-headless-5.3.3-el7.x86_64.rpm
+
+sudo tee /etc/appgate.conf <<EOF >/dev/null
+[Settings]
+LogLevel = Info
+
+[Credentials]
+ControllerUrl = ${1}
+Username = ${2}
+Password = ${3}
+EOF
+sudo appgate_service_configurator reload && \
+sudo appgate_service_configurator status
+echo "controllerURL $1"
+echo "username $2"
+echo "password $3"
+time=sudo appgate_service_configurator status
 echo "::set-output name=secret-value::$time"
