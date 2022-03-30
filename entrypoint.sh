f373e97dfde2854c5ed8bff335590611e7a2ce7e
diego-bellon <difer2085@gmail.com>
2022-03-30 16:55:09 -0500

    fix: move configuration to dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,4 @@
 #!/bin/sh -l
-tee /etc/appgate.conf <<EOF >/dev/null
-[Settings]
-LogLevel = Info
-[Credentials]
-ControllerUrl = ${1}
-Username = ${2}
-Password = ${3}
-EOF
-appgate_service_configurator reload && \
-appgate_service_configurator status
 echo "controllerURL $1"
 echo "username $2"
 echo "password $3"
