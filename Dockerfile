17c634de22ed39757b39748f88472b72001840c9
diego-bellon <difer2085@gmail.com>
2022-03-30 14:14:07 -0500

    fix: SDP configuration in container

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,10 @@
 FROM amazonlinux
+RUN  amazon-linux-extras install -y epel && \
+     yum install -y https://repo.ius.io/ius-release-el7.rpm && \
+     yum install -y dnsmasq python36 python36-pip python36-dbus wget && \
+     wget https://bin.appgate-sdp.com/5.3/client/appgate-sdp-headless-5.3.3-el7.x86_64.rpm && \
+     rpm --install --nodeps ./appgate-sdp-headless-5.3.3-el7.x86_64.rpm
+
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod a+x /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,14 @@
 #!/bin/sh -l
-sudo amazon-linux-extras install -y epel &&\
-sudo yum install -y https://repo.ius.io/ius-release-el7.rpm && \
-sudo yum install dnsmasq python36 python36-pip python36-dbus && \
-wget https://bin.appgate-sdp.com/5.3/client/appgate-sdp-headless-5.3.3-el7.x86_64.rpm && \
-sudo rpm --install -y --nodeps ./appgate-sdp-headless-5.3.3-el7.x86_64.rpm
-
 sudo tee /etc/appgate.conf <<EOF >/dev/null
 [Settings]
 LogLevel = Info
-
 [Credentials]
 ControllerUrl = ${1}
 Username = ${2}
 Password = ${3}
 EOF
-sudo appgate_service_configurator reload && \
-sudo appgate_service_configurator status
+RUN sudo appgate_service_configurator reload && \
+    sudo appgate_service_configurator status
 echo "controllerURL $1"
 echo "username $2"
 echo "password $3"
